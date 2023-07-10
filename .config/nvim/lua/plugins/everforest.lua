ecb4e1d2d2b3bb5bd69e7c4ac30472d5ad907449
Nar Cuenca <narc.ph@gmail.com>
2023-07-10 09:53:28 +0800

    added neovim config

---
+++ .config/nvim/lua/plugins/everforest.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    name = "everforest",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require("everforest").setup({
+        -- Your config here
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
